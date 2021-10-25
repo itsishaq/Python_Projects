6b3d3bbff14b4c15fccbf83df6177cbb9025e35f
itsishaq <91102804+itsishaq@users.noreply.github.com>
2021-10-25 20:15:37 +0530

    Update and rename Clock_program to Clock_program.py

--- Clock_program.py
+++ Clock_program.py
@@ -1,17 +1,26 @@
 #A clock program to setup.
+
 from tkinter import *
+
 from tkinter.ttk import *
 
 from time import strftime
+
 root = Tk()
+
 root.title("CLOCK")
 
 def time():
     str1 = strftime('%H:%M:%S %p')
+    
     label.config(text=str1)
+    
     label.after(1000,time)
 
 label = Label(root,font=("ds-digital",100),background="red",foreground="black")
+
 label.pack(anchor='center')
+
 time()
+
 mainloop()

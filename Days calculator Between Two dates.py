54090e68d2938b6874c4c42033055fb5b78e39dd
Ishaq Mohamed <91102804+itsishaq@users.noreply.github.com>
2022-01-06 11:09:16 +0530

    Create Days calculator Between Two dates.py

---
+++ Days calculator Between Two dates.py
@@ -0,0 +1,18 @@
+import datetime
+from dateutil import relativedelta
+
+# Create two imaginary or desired dates
+date1 = datetime.date(2003,6,6)
+date2 = datetime.date(2021,12,5)
+
+# Using relativedelta find the difference between the recent date
+# and past date.
+diff = relativedelta.relativedelta(date2,date1)
+
+# Using datetime module find the years, months, days between those two dates
+years = diff.years
+months = diff.months
+days = diff.days
+
+# Print the output using print statement by formatting values
+print('{} years {} months {} days'.format(years,months,days))
